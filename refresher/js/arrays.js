034a59ff06fc42db6a5e300e855abe7ee6af69b9
Chris Li <hychrisli@users.noreply.github.com>
2018-02-10 09:13:33 -0800

    arrays

---
+++ refresher/js/arrays.js
@@ -0,0 +1,35 @@
+/**
+*
+* Array operations. Check if it's array, if an array is empty
+* Append and delete items from the array.
+*
+* */
+
+
+let a = [];
+let b = ['door', 'desk'];
+const assert = require('assert');
+
+describe('arrays', function(){
+
+    it ('Should be array', function(){
+        assert.equal(a.constructor === Array, true);
+    })
+
+    it ('Should be empty now', function(){
+        assert.equal(a.length, 0)
+    })
+
+    it ('Should be appended with a value', function(){
+        a.push('chair');
+        assert.equal(a.length, 1);
+        assert.equal(a[0], 'chair')
+    })
+
+    it ('Should be concatenated a and b', function(){
+        let c = a.concat(b);
+        assert.equal(c.length, 3);
+        assert.equal(c[1], 'door');
+    })
+
+})
