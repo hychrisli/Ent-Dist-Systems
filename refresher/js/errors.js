b6412a78a77485f0a11b25a9282298983e6066a4
Chris Li <hychrisli@users.noreply.github.com>
2018-02-13 15:03:26 -0800

    errors

---
+++ refresher/js/errors.js
@@ -0,0 +1,25 @@
+/**
+ *
+ *
+ *
+ */
+
+
+const chai = require('chai');
+const AppError = new Error("Application Error");
+
+function error(){
+    try{
+        eval("2++3");
+    }catch(SyntaxError){
+        throw AppError
+    }
+}
+
+describe('Errors', function(){
+
+    it("Should be Application Eror", function(){
+        chai.expect(error).to.throw(AppError);
+    });
+
+})
