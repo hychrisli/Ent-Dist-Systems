18908fb0b93e8ef57b32776d21c41762a795d81c
Chris Li <hychrisli@users.noreply.github.com>
2018-02-26 15:13:26 -0800

    initial fun

--- ui-demo/src/App.js
+++ ui-demo/src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+
+import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './users';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
+        <Resource name="users" list={UserList}/>
+      </Admin>
     );
   }
 }
